ae247f20832efb7c94eeea3706d6190a50f366f3
AdamDonegan <adamjdonegan@gmail.com>
2015-10-26 16:28:42 -0400

    Changed all GETs to cleaner HttpRequest get method

--- src/com/adamdonegan/Discogs4J/util/Test.java
+++ src/com/adamdonegan/Discogs4J/util/Test.java
@@ -7,16 +7,18 @@ import com.adamdonegan.Discogs4J.client.DiscogsClient;
 public class Test {
 	private static String CONSUMER_KEY = "tZplWaLrLakbPmeKDnNR";
 	private static String CONSUMER_SECRET = "WlvAHSrMKkEokrhICslQndFmlwjafEwW";
+	private static String USER_AGENT = "Discogs4J/0.1 +https://github.com/ajdons/Discogs4J";
+
 	public static void main(String[] args) throws JSONException {
 		System.out.println("This is a test");
-		DiscogsClient client = new DiscogsClient(CONSUMER_KEY, CONSUMER_SECRET);
+		DiscogsClient client = new DiscogsClient(CONSUMER_KEY, CONSUMER_SECRET, USER_AGENT);
 		//client.getRequestToken();
 		//client.getAccessToken();
 		
 		//client.identity();
 		//client.profile("discogs4jdev");
 		
-		//client.getReleaseById("249504");
+		//client.release("6160782");
 		//client.masterRelease("671582");
 		//client.masterReleaseVersions("671582");
 		//client.artist("732448");
@@ -28,8 +30,8 @@ public class Test {
 		//client.collection("discogs4jdev");
 		//client.collectionFolder("discogs4jdev", "1");
 		//client.collectionReleases("discogs4jdev", "1");
-		client.addReleaseToFolder("discogs4jdev", "633608", "6160782");
-		//client.modifyInstanceInFolder("discogs4jdev", "1", "6160782", "141811555");
+		//client.addReleaseToFolder("discogs4jdev", "1", "6160782");
+		//client.modifyInstanceInFolder("discogs4jdev", "1", "6160782", "143245571");
 	}
 }
 
